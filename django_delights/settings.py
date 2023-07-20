bb2be30358e5c92b2495980a26186249829a9cd2
RobMcI <114496674+RobertMcIsaac@users.noreply.github.com>
2023-07-20 17:02:34 +0100

    Changed login view to inbuilt, changed login form to use form.as_p. added logout view

--- django_delights/settings.py
+++ django_delights/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY =  os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -104,6 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_REDIRECT_URL = "home"
+
+LOGOUT_REDIRECT_URL = "home"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
--- inventory/views.py
+++ inventory/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import views as auth_views
 
-# built-in login view 
+# LOGIN VIEW (built-in)
+class InventoryLoginView(auth_views.LoginView):
+    template_name = "registration/login.html"
+    redirect_authenticated_user = True
 
-# LOGIN VIEW
-def login_view(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect("home")
-        else:
-            return render(request, "registration/login.html")
+# # LOGIN VIEW (custom)
+# def login_view(request):
+#     if request.method == "POST":
+#         username = request.POST["username"]
+#         password = request.POST["password"]
+#         user = authenticate(request, username=username, password=password)
+#         if user is not None:
+#             login(request, user)
+#             return redirect("home")
+#         else:
+#             return render(request, "registration/login.html")
+
 
 # HOME VIEW
 def home_view(request):
