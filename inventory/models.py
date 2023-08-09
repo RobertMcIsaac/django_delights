1b45d94d90053f7ec5dd39c69b1bd572cf9477cf
RobMcI <114496674+RobertMcIsaac@users.noreply.github.com>
2023-08-09 12:25:39 +0100

    changed max_digits and decimal_places for model fields

--- inventory/models.py
+++ inventory/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class Ingredient(models.Model):
     name = models.CharField(max_length=100, unique=True)
     cost_per_unit = models.DecimalField(max_digits=5, decimal_places=2, default=0)
-    quantity_available = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    quantity_available = models.DecimalField(max_digits=6, decimal_places=3, default=0)
     # Define constants at class level for MEASUREMENT_UNIT_CHOICES
     GRAMS = "GR"
     KILOGRAMS = "KG"
@@ -42,7 +42,7 @@ class Ingredient(models.Model):
 # MenuItem MODEL
 class MenuItem(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    price = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=0)
 
     def __str__(self):
         return self.name
@@ -56,7 +56,7 @@ class MenuItem(models.Model):
 class RecipeRequirement(models.Model):
     menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.PROTECT)
-    ingredient_quantity = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    ingredient_quantity = models.DecimalField(max_digits=6, decimal_places=3, default=0)
 
     def __str__(self):
         return str(self.menuitem) + " Recipe"
